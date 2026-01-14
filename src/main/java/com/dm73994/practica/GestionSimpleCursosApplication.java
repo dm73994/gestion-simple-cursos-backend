b319db99ce2557c4b13b315510181a0a0656da0f
dm73994 <dm31284@gmail.com>
2026-01-14 15:06:56 -0500

    Iniciar proyecto y configuración básica

---
+++ src/main/java/com/dm73994/practica/GestionSimpleCursosApplication.java
@@ -0,0 +1,13 @@
+package com.dm73994.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionSimpleCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionSimpleCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dm73994/practica/GestionSimpleCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dm73994.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionSimpleCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
